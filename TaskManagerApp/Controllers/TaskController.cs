14c84ad0a9eb9d4a617a8bb2d98140e388d04a54
drunde1 <androxa228@gmail.com>
2024-05-14 21:44:35 +0300

    first commit

---
+++ TaskManagerApp/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private static readonly string[] Names = new[]
+        {
+            "Cleaning", "Chiling", "Meeting", "HangingOut", "Film", "Work", "Homework"
+        };
+
+        private static readonly string[] Descriptions = new[]
+       {
+            "MustBeDone", "By16.05", "CanBeForgoten", "SomeForFun", "NotMine"
+        };
+
+        private readonly ILogger<Task> _logger;
+
+        public TaskController(ILogger<Task> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<Task> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Task
+            {
+                Name = Names[Random.Shared.Next(Names.Length)],
+                Description = Descriptions[Random.Shared.Next(Descriptions.Length)],
+                IsCompleted = false
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ TaskManagerApp/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagerApp
+{
+    public class Task
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
